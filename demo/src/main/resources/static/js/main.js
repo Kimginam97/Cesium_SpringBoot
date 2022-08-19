87200e63cf0a4e5c46b655014a278a7a35411a64
Kimginam97 <rlarlska79@naver.com>
2022-08-19 13:52:20 +0900

    좌우화면분할 미완성

--- demo/src/main/resources/static/js/main.js
+++ demo/src/main/resources/static/js/main.js
@@ -18,8 +18,8 @@ addEventListener('load', () => {
         37.66017,
         2200
     );
-    const camera = viewer.scene.camera;
 
+    const camera = viewer.scene.camera;
     viewer.camera.flyTo({
         destination: destination,
     });
@@ -27,52 +27,63 @@ addEventListener('load', () => {
     // 확대
     const zoomIn = document.querySelector('.zoom-in');
     zoomIn.addEventListener('click', () => {
-        camera.zoomIn(500);
+        camera.zoomIn(700);
     });
 
     // 축소
     const zoomOut = document.querySelector('.zoom-out');
     zoomOut.addEventListener('click', () => {
-        camera.zoomOut(500);
+        camera.zoomOut(700);
     });
 
     // 왼쪽 회전
     const rotateLeft = document.querySelector('.rotate-left');
     rotateLeft.addEventListener('click', () => {
-        camera.rotate(destination, 90);
+        camera.rotate(destination, 45);
     });
 
     // 오른쪽 회전
     const rotateRight = document.querySelector('.rotate-right');
     rotateRight.addEventListener('click', () => {
-        camera.rotate(destination, -90);
+        camera.rotate(destination, -45);
     });
 
-    const layers = viewer.imageryLayers;
-
     // 좌우화면 분할
-    const leftRightDivision = document.querySelector('.left-right_division');
-    leftRightDivision.addEventListener('click', () => {
+    const layers = viewer.imageryLayers;
+    const leftRightDivisionBtn = document.querySelector('.left-right_division');
+    const slider = document.querySelector('.slider');
+    const kakaoImg = document.querySelector('.kakaoimg');
+    const sliderHandler = new Cesium.ScreenSpaceEventHandler(slider);
+
+    leftRightDivisionBtn.addEventListener('click', onSlider);
+    kakaoImg.addEventListener('click', () => {
         const earthAtNight = layers.addImageryProvider(
             new Cesium.IonImageryProvider({ assetId: 4 })
         );
         earthAtNight.splitDirection = Cesium.SplitDirection.LEFT; // Only show to the left of the slider.
-
         // Sync the position of the slider with the split position
-        const slider = document.getElementById('slider');
-        slider.style.display = 'block';
         viewer.scene.splitPosition =
             slider.offsetLeft / slider.parentElement.offsetWidth;
+    });
 
-        const handler = new Cesium.ScreenSpaceEventHandler(slider);
+    function onSlider() {
+        const hasClass = slider.classList.contains('on');
+        if (!hasClass) {
+            slider.classList.add('on');
+            sliderEventHandler();
+        } else {
+            slider.classList.remove('on');
+            layers.removeAll();
+            console.log(layers);
+        }
+    }
 
+    function sliderEventHandler() {
         let moveActive = false;
-
         function move(movement) {
             if (!moveActive) {
                 return;
             }
-
             const relativeOffset = movement.endPosition.x;
             const splitPosition =
                 (slider.offsetLeft + relativeOffset) /
@@ -80,22 +91,25 @@ addEventListener('load', () => {
             slider.style.left = `${100.0 * splitPosition}%`;
             viewer.scene.splitPosition = splitPosition;
         }
-
-        handler.setInputAction(function () {
+        sliderHandler.setInputAction(function () {
             moveActive = true;
         }, Cesium.ScreenSpaceEventType.LEFT_DOWN);
-        handler.setInputAction(function () {
+        sliderHandler.setInputAction(function () {
             moveActive = true;
         }, Cesium.ScreenSpaceEventType.PINCH_START);
-
-        handler.setInputAction(move, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
-        handler.setInputAction(move, Cesium.ScreenSpaceEventType.PINCH_MOVE);
-
-        handler.setInputAction(function () {
+        sliderHandler.setInputAction(
+            move,
+            Cesium.ScreenSpaceEventType.MOUSE_MOVE
+        );
+        sliderHandler.setInputAction(
+            move,
+            Cesium.ScreenSpaceEventType.PINCH_MOVE
+        );
+        sliderHandler.setInputAction(function () {
             moveActive = false;
         }, Cesium.ScreenSpaceEventType.LEFT_UP);
-        handler.setInputAction(function () {
+        sliderHandler.setInputAction(function () {
             moveActive = false;
         }, Cesium.ScreenSpaceEventType.PINCH_END);
-    });
+    }
 });
